42f1d2ddbf529ab3dfa2a8a9ac89871213cd60a5
AAYUSH DUBEY <aayush200dubey@gmail.com>
2024-10-13 23:08:32 +0530

    Add files via upload

---
+++ src/utils/EventDatabase.jsx
@@ -0,0 +1,49 @@
+export const eventList = [
+    {
+      id: 1,
+      heading: "Arijit Singh Concert",
+      date: { year: 2024, month: "November" },
+      location: "Indore, India",
+      description:
+        "Join us for an unforgettable evening with the melodious Arijit Singh, one of India's most beloved playback singers. This concert, taking place in Indore in November 2024, promises to be a spectacular celebration of music, featuring Arijit's enchanting voice and heartfelt performances!",
+      img: "https://res.cloudinary.com/dwzmsvp7f/image/upload/q_75,f_auto,w_600/c_crop%2Cg_custom%2Fv1490081836%2Fglwnxzo4zcaguytyglvf.jpg",
+    },
+    {
+      id: 2,
+      heading: "Mic Drop With Samay Raina",
+      date: { year: 2025, month: "January" },
+      location: "Delhi, India",
+      description:
+        "Get ready for a night filled with laughter and joy as renowned stand-up comedian Samay Raina takes the stage in Delhi this January 2025. Known for his sharp wit, relatable humor, and engaging storytelling, Samay promises to deliver an unforgettable comedy experience that will leave you in splits!",
+      img: "https://www.shoutlo.com/uploads/articles/header-img-1682406980-samay-raina-performing-live-comedy-show.jpg",
+    },
+    {
+      id: 3,
+      heading: "Bhuvan Bam Meetup ",
+      date: { year: 2025, month: "March" },
+      location: "Bangalore, India",
+      description:
+        "Join us for an exciting and entertaining meetup with Bhuvan Bam, one of India's most popular YouTube comedy sketch creators, in Bangalore this March 2025. Known for his unique humor and relatable content, Bhuvan has captured the hearts of millions, and this event is an opportunity for fans and aspiring creators to connect with him and each other!",
+      img: "https://www.leisurebyte.com/wp-content/uploads/2022/11/Dhindora.jpg",
+    },
+    {
+      id: 4,
+      heading: "Sandeep Maheshwari",
+      date: { year: 2025, month: "June" },
+      location: "Mumbai, India",
+      description:
+        "Join us for an inspiring session with renowned motivational speaker and entrepreneur Sandeep Maheshwari in Mumbai this June 2025. Known for his impactful talks and practical insights, Sandeep aims to empower individuals to overcome obstacles, pursue their passions, and lead fulfilling lives!",
+      img: "https://xeebook.com/wp-content/uploads/2023/09/Sandeep-Maheshwari.webp",
+    },
+    {
+      id: 5,
+      heading: "ISRO: Gagan yaan Mission",
+      date: { year: 2025, month: "September" },
+      location: "Andhra Pradesh, India",
+      description:
+        "Join us for a historic moment in India's space exploration journey as the Indian Space Research Organisation (ISRO) prepares for the launch of the Gaganyaan program in September 2025. This ambitious initiative marks India's first crewed space mission, aimed at demonstrating the country's capabilities in human spaceflight. The launch will take place at the Satish Dhawan Space Centre in Sriharikota, Andhra Pradesh, promising a spectacular event for space enthusiasts and the general public alike!",
+      img: "https://i0.wp.com/www.defencexp.com/wp-content/uploads/2022/05/284827258_507084827869043_5595838118511789328_n.jpg?fit=1200%2C675&ssl=1",
+    },
+    
+  ];
+  
